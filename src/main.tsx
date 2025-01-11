01a81bdb660f7cff2093bca0e0b942d0c6eb3b50
techking11 <nma1132000@gmail.com>
2025-01-11 22:25:25 +0700

    add redux toolkit and react query configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 // import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import App from './App.tsx';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store.ts';
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   // <StrictMode>
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </QueryClientProvider>
   // </StrictMode>
 );
