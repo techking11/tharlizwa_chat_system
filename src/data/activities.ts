f39fb7a08d71fbbe38f5bd18a5f0383bcd81d093
techking11 <nma1132000@gmail.com>
2025-01-15 21:26:53 +0700

    data: add activities

---
+++ src/data/activities.ts
@@ -0,0 +1,767 @@
+export default [
+  {
+    code: 'U+1F383',
+    character: '🎃',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F383.png',
+    name: 'jack-o-lantern',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F384',
+    character: '🎄',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F384.png',
+    name: 'Christmas tree',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F386',
+    character: '🎆',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F386.png',
+    name: 'fireworks',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F387',
+    character: '🎇',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F387.png',
+    name: 'sparkler',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F9E8',
+    character: '🧨',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F9E8.png',
+    name: 'firecracker',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+2728',
+    character: '✨',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B2728.png',
+    name: 'sparkles',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F388',
+    character: '🎈',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F388.png',
+    name: 'balloon',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F389',
+    character: '🎉',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F389.png',
+    name: 'party popper',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F38A',
+    character: '🎊',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F38A.png',
+    name: 'confetti ball',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F38B',
+    character: '🎋',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F38B.png',
+    name: 'tanabata tree',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F38D',
+    character: '🎍',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F38D.png',
+    name: 'pine decoration',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F38E',
+    character: '🎎',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F38E.png',
+    name: 'Japanese dolls',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F38F',
+    character: '🎏',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F38F.png',
+    name: 'carp streamer',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F390',
+    character: '🎐',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F390.png',
+    name: 'wind chime',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F391',
+    character: '🎑',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F391.png',
+    name: 'moon viewing ceremony',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F9E7',
+    character: '🧧',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F9E7.png',
+    name: 'red envelope',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F380',
+    character: '🎀',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F380.png',
+    name: 'ribbon',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F381',
+    character: '🎁',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F381.png',
+    name: 'wrapped gift',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F397',
+    character: '🎗',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F397.png',
+    name: 'reminder ribbon',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F39F',
+    character: '🎟',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F39F.png',
+    name: 'admission tickets',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F3AB',
+    character: '🎫',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3AB.png',
+    name: 'ticket',
+    group: 'activities',
+    subgroup: 'event',
+  },
+  {
+    code: 'U+1F396',
+    character: '🎖',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F396.png',
+    name: 'military medal',
+    group: 'activities',
+    subgroup: 'award_medal',
+  },
+  {
+    code: 'U+1F3C6',
+    character: '🏆',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3C6.png',
+    name: 'trophy',
+    group: 'activities',
+    subgroup: 'award_medal',
+  },
+  {
+    code: 'U+1F3C5',
+    character: '🏅',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3C5.png',
+    name: 'sports medal',
+    group: 'activities',
+    subgroup: 'award_medal',
+  },
+  {
+    code: 'U+1F947',
+    character: '🥇',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F947.png',
+    name: '1st place medal',
+    group: 'activities',
+    subgroup: 'award_medal',
+  },
+  {
+    code: 'U+1F948',
+    character: '🥈',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F948.png',
+    name: '2nd place medal',
+    group: 'activities',
+    subgroup: 'award_medal',
+  },
+  {
+    code: 'U+1F949',
+    character: '🥉',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F949.png',
+    name: '3rd place medal',
+    group: 'activities',
+    subgroup: 'award_medal',
+  },
+  {
+    code: 'U+26BD',
+    character: '⚽',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B26BD.png',
+    name: 'soccer ball',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+26BE',
+    character: '⚾',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B26BE.png',
+    name: 'baseball',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F94E',
+    character: '🥎',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F94E.png',
+    name: 'softball',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3C0',
+    character: '🏀',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3C0.png',
+    name: 'basketball',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3D0',
+    character: '🏐',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3D0.png',
+    name: 'volleyball',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3C8',
+    character: '🏈',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3C8.png',
+    name: 'american football',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3C9',
+    character: '🏉',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3C9.png',
+    name: 'rugby football',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3BE',
+    character: '🎾',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3BE.png',
+    name: 'tennis',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F94F',
+    character: '🥏',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F94F.png',
+    name: 'flying disc',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3B3',
+    character: '🎳',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3B3.png',
+    name: 'bowling',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3CF',
+    character: '🏏',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3CF.png',
+    name: 'cricket game',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3D1',
+    character: '🏑',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3D1.png',
+    name: 'field hockey',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3D2',
+    character: '🏒',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3D2.png',
+    name: 'ice hockey',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F94D',
+    character: '🥍',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F94D.png',
+    name: 'lacrosse',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3D3',
+    character: '🏓',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3D3.png',
+    name: 'ping pong',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3F8',
+    character: '🏸',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3F8.png',
+    name: 'badminton',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F94A',
+    character: '🥊',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F94A.png',
+    name: 'boxing glove',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F94B',
+    character: '🥋',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F94B.png',
+    name: 'martial arts uniform',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F945',
+    character: '🥅',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F945.png',
+    name: 'goal net',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+26F3',
+    character: '⛳',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B26F3.png',
+    name: 'flag in hole',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+26F8',
+    character: '⛸',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B26F8.png',
+    name: 'ice skate',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3A3',
+    character: '🎣',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3A3.png',
+    name: 'fishing pole',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F93F',
+    character: '🤿',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F93F.png',
+    name: 'diving mask',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3BD',
+    character: '🎽',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3BD.png',
+    name: 'running shirt',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3BF',
+    character: '🎿',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3BF.png',
+    name: 'skis',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F6F7',
+    character: '🛷',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F6F7.png',
+    name: 'sled',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F94C',
+    character: '🥌',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F94C.png',
+    name: 'curling stone',
+    group: 'activities',
+    subgroup: 'sport',
+  },
+  {
+    code: 'U+1F3AF',
+    character: '🎯',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3AF.png',
+    name: 'bullseye',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1FA80',
+    character: '🪀',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1FA80.png',
+    name: 'yo-yo',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1FA81',
+    character: '🪁',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1FA81.png',
+    name: 'kite',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F52B',
+    character: '🔫',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F52B.png',
+    name: 'water pistol',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F3B1',
+    character: '🎱',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3B1.png',
+    name: 'pool 8 ball',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F52E',
+    character: '🔮',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F52E.png',
+    name: 'crystal ball',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1FA84',
+    character: '🪄',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1FA84.png',
+    name: 'magic wand',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F3AE',
+    character: '🎮',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3AE.png',
+    name: 'video game',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F579',
+    character: '🕹',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F579.png',
+    name: 'joystick',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F3B0',
+    character: '🎰',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3B0.png',
+    name: 'slot machine',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F3B2',
+    character: '🎲',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3B2.png',
+    name: 'game die',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F9E9',
+    character: '🧩',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F9E9.png',
+    name: 'puzzle piece',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F9F8',
+    character: '🧸',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F9F8.png',
+    name: 'teddy bear',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1FA85',
+    character: '🪅',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1FA85.png',
+    name: 'piñata',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1FAA9',
+    character: '🪩',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1FAA9.png',
+    name: 'mirror ball',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1FA86',
+    character: '🪆',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1FA86.png',
+    name: 'nesting dolls',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+2660',
+    character: '♠',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B2660.png',
+    name: 'spade suit',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+2665',
+    character: '♥',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B2665.png',
+    name: 'heart suit',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+2666',
+    character: '♦',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B2666.png',
+    name: 'diamond suit',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+2663',
+    character: '♣',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B2663.png',
+    name: 'club suit',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+265F',
+    character: '♟',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B265F.png',
+    name: 'chess pawn',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F0CF',
+    character: '🃏',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F0CF.png',
+    name: 'joker',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F004',
+    character: '🀄',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F004.png',
+    name: 'mahjong red dragon',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F3B4',
+    character: '🎴',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3B4.png',
+    name: 'flower playing cards',
+    group: 'activities',
+    subgroup: 'game',
+  },
+  {
+    code: 'U+1F3AD',
+    character: '🎭',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3AD.png',
+    name: 'performing arts',
+    group: 'activities',
+    subgroup: 'arts_crafts',
+  },
+  {
+    code: 'U+1F5BC',
+    character: '🖼',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F5BC.png',
+    name: 'framed picture',
+    group: 'activities',
+    subgroup: 'arts_crafts',
+  },
+  {
+    code: 'U+1F3A8',
+    character: '🎨',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3A8.png',
+    name: 'artist palette',
+    group: 'activities',
+    subgroup: 'arts_crafts',
+  },
+  {
+    code: 'U+1F9F5',
+    character: '🧵',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F9F5.png',
+    name: 'thread',
+    group: 'activities',
+    subgroup: 'arts_crafts',
+  },
+  {
+    code: 'U+1FAA1',
+    character: '🪡',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1FAA1.png',
+    name: 'sewing needle',
+    group: 'activities',
+    subgroup: 'arts_crafts',
+  },
+  {
+    code: 'U+1F9F6',
+    character: '🧶',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F9F6.png',
+    name: 'yarn',
+    group: 'activities',
+    subgroup: 'arts_crafts',
+  },
+  {
+    code: 'U+1FAA2',
+    character: '🪢',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1FAA2.png',
+    name: 'knot',
+    group: 'activities',
+    subgroup: 'arts_crafts',
+  },
+];
