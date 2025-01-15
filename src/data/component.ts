c8feee1193c080f5f7580a8e18144a514c5c113b
techking11 <nma1132000@gmail.com>
2025-01-15 21:27:28 +0700

    data: add component

---
+++ src/data/component.ts
@@ -0,0 +1,83 @@
+export default [
+  {
+    code: 'U+1F9B0',
+    character: '🦰',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F9B0.png',
+    name: 'red hair',
+    group: 'component',
+    subgroup: 'hair_style',
+  },
+  {
+    code: 'U+1F9B1',
+    character: '🦱',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F9B1.png',
+    name: 'curly hair',
+    group: 'component',
+    subgroup: 'hair_style',
+  },
+  {
+    code: 'U+1F9B3',
+    character: '🦳',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F9B3.png',
+    name: 'white hair',
+    group: 'component',
+    subgroup: 'hair_style',
+  },
+  {
+    code: 'U+1F9B2',
+    character: '🦲',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F9B2.png',
+    name: 'bald',
+    group: 'component',
+    subgroup: 'hair_style',
+  },
+  {
+    code: 'U+1F3FB',
+    character: '🏻',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3FB.png',
+    name: 'light skin tone',
+    group: 'component',
+    subgroup: 'skin_tone',
+  },
+  {
+    code: 'U+1F3FC',
+    character: '🏼',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3FC.png',
+    name: 'medium-light skin tone',
+    group: 'component',
+    subgroup: 'skin_tone',
+  },
+  {
+    code: 'U+1F3FD',
+    character: '🏽',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3FD.png',
+    name: 'medium skin tone',
+    group: 'component',
+    subgroup: 'skin_tone',
+  },
+  {
+    code: 'U+1F3FE',
+    character: '🏾',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3FE.png',
+    name: 'medium-dark skin tone',
+    group: 'component',
+    subgroup: 'skin_tone',
+  },
+  {
+    code: 'U+1F3FF',
+    character: '🏿',
+    image:
+      'https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F3FF.png',
+    name: 'dark skin tone',
+    group: 'component',
+    subgroup: 'skin_tone',
+  },
+];
