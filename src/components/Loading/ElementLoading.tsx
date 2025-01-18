b1cf4324ec2ca60ef105ba83b527cbed1c36739e
techking11 <nma1132000@gmail.com>
2025-01-18 20:14:32 +0700

    component: add ElementLoading, PageLoading

---
+++ src/components/Loading/ElementLoading.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ThreeDot } from 'react-loading-indicators';
+
+function ElementLoading(): React.ReactElement {
+  return <ThreeDot color="#9da09d" size="small" text="Loading" textColor="" />;
+}
+
+export default ElementLoading;
---
+++ src/components/Loading/PageLoading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ThreeDot } from 'react-loading-indicators';
+
+function PageLoading(): React.ReactElement {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <ThreeDot color="#9da09d" size="large" text="Loading" textColor="" />;
+    </div>
+  );
+}
+
+export default PageLoading;
