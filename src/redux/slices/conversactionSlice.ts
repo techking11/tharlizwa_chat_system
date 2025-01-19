148b9bf5ed4206b190db5d4f9dd825d48502641c
techking11 <nma1132000@gmail.com>
2025-01-19 22:46:24 +0700

    slice: add conversactionSlice

---
+++ src/redux/slices/conversactionSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import conversactions from '../../schemas/conversactionSchema';
+
+interface ConversactionItem {
+  id: number;
+  is_group?: boolean;
+  name?: string;
+  created_by?: number;
+  created_at?: string;
+  updated_at?: string;
+  is_deleted?: boolean;
+}
+
+const initialState: ConversactionItem[] = conversactions;
+
+const conversactionSlice = createSlice({
+  name: 'conversaction',
+  initialState,
+  reducers: {
+    getAll(state) {
+      return state.filter((item) => item.is_deleted !== true) || null;
+    },
+    createOne(state, actions: PayloadAction<ConversactionItem>) {
+      state.push(actions.payload);
+    },
+    getOne(state, actions: PayloadAction<number>) {
+      return state.filter((item) => item.id === actions.payload) || null;
+    },
+    updateOne(state, actions: PayloadAction<ConversactionItem>) {
+      const index = state.findIndex((item) => item.id === actions.payload.id);
+      if (index !== -1) {
+        state[index] = { ...state[index], ...actions.payload };
+      }
+    },
+    deleteOne(state, actions: PayloadAction<number>) {
+      const item = state.find((item) => item.id === actions.payload);
+      if (item) {
+        item.is_deleted = true;
+      }
+    },
+  },
+});
+
+export const { getAll, createOne, getOne, updateOne, deleteOne } =
+  conversactionSlice.actions;
+export default conversactionSlice.reducer;
