d41d2f1214cfc6da3633167c1b6dbb9ed7c6a106
techking11 <nma1132000@gmail.com>
2025-01-20 23:29:32 +0700

    type: add conversactionType

---
+++ src/types/conversactionTypes.ts
@@ -0,0 +1,9 @@
+export interface ConversactionItem {
+  id: number;
+  is_group: boolean;
+  name: string;
+  created_by: number;
+  created_at: string;
+  updated_at: string;
+  is_deleted: boolean;
+}
