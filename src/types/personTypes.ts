f5716420302445d7b1c14fb3ebe0df61b6478e63
techking11 <nma1132000@gmail.com>
2025-01-20 23:30:53 +0700

    type: add personType

---
+++ src/types/personTypes.ts
@@ -0,0 +1,12 @@
+export interface Person {
+  id: number;
+  name: string;
+  phone_no: string;
+  email: string;
+  password: string;
+  profile: string;
+  status: string;
+  created_at: string;
+  updated_at: string;
+  is_deleted: boolean;
+}
