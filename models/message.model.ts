28c1871a9f913a137533693498b223117dda0438
techking11 <nma1132000@gmail.com>
2025-01-22 23:31:47 +0700

    model: add messageModal, roomModal, userModal

---
+++ models/message.model.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IMessage extends Document {
+  senderId: string;
+  text: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema = new Schema({
+  roomId: { type: String, required: true },
+  senderId: { type: String, required: true },
+  username: { type: String, required: true },
+  text: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const Message = mongoose.model<IMessage>('Message', MessageSchema);
--- models/user.model.ts
+++ models/user.model.ts
@@ -1,22 +1,15 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
   name: string;
-  phone_no: string;
-  email: string;
-  password: string;
-  profile: string;
-  status: 'Offline' | 'Online' | 'Busy' | 'Focus' | 'Away';
-  socket_id: string;
-  created_at: Date;
-  updated_at: Date;
-  is_deleted: boolean;
+  socketId: string;
+  createdAt: Date;
 }
 
 const UserSchema: Schema = new Schema({
-  name: { type: String, required: true, min: 5 },
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date },
+  name: { type: String, required: true },
+  socketId: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 export const User = mongoose.model<IUser>('User', UserSchema);
